2a65fc43a4dad2ee8a557ff010d69802b25f1377
yanjian <jian.yan@ishansong.com>
2021-01-04 13:56:10 +0800

    完成install逻辑

---
+++ vue-router/index.js
@@ -0,0 +1,9 @@
+import install from "./install";
+
+class VueRouter {
+  constructor(options) {}
+}
+
+VueRouter.install = install;
+
+export default VueRouter;
---
+++ vue-router/install.js
@@ -0,0 +1,26 @@
+export let _Vue;
+import RouterLink from "./components/link";
+import RouterView from "./components/view";
+
+function install(Vue, options) {
+  _Vue = Vue;
+
+  Vue.component("router-link", RouterLink);
+  Vue.component("router-view", RouterView);
+  Vue.mixin({
+    beforeCreate() {
+      // 根实例，new vue出的那个实例
+      if (this.$options.router) {
+        this._routerRoot = this;
+        this._router = this.$options.router;
+      } else {
+        this._routerRoot = this.$parent && this.$parent._routerRoot;
+      }
+    },
+  });
+
+  Vue.prototype.$router = {};
+  Vue.prototype.$route = {};
+}
+
+export default install;
